c509d2e4534ec01684863db86b0e4f6a10a29cf7
loboda4450 <loboda4450@gmail.com>
2023-01-16 15:14:30 +0100

    automatic token refresh, ensuring that user wants to open compartment

--- main.py
+++ main.py
@@ -12,6 +12,61 @@ from inpost.static.exceptions import *
 from inpost.api import Inpost
 
 
+async def send_pcgs(event, inp, status):
+    p: List[Parcel] = await inp[event.sender.id].get_parcels(status=status, parse=True)
+    if len(p) > 0:
+        for package in p:
+            await event.reply(f'Sender: {package.sender.sender_name}\n'
+                              f'Shipment number: {package.shipment_number}\n'
+                              f'Status: {package.status.value}\n'
+                              f'Pickup point: {package.pickup_point}',
+                              buttons=[
+                                  [Button.inline('Open Code'),
+                                   Button.inline('QR Code')],
+                                  [Button.inline(
+                                      'Open Compartment')]] if package.status != ParcelStatus.DELIVERED else
+                              [Button.inline('Open Code'),
+                               Button.inline('QR Code')]
+                              )
+    else:
+        if isinstance(event, CallbackQuery.Event):
+            await event.answer('No parcels with specified status!', alert=True)
+        elif isinstance(event, NewMessage.Event):
+            await event.reply('No parcels with specified status!')
+
+    return status
+
+
+async def send_qrc(event, inp, shipment_number):
+    p: Parcel = await inp[event.sender.id].get_parcel(shipment_number=shipment_number, parse=True)
+    if p.status == ParcelStatus.READY_TO_PICKUP:
+        await event.reply(file=p.generate_qr_image)
+    else:
+        await event.answer(f'Parcel not ready for pickup!\nStatus: {p.status.value}', alert=True)
+
+
+async def show_oc(event, inp, shipment_number):
+    p: Parcel = await inp[event.sender.id].get_parcel(shipment_number=shipment_number, parse=True)
+    if p.status == ParcelStatus.READY_TO_PICKUP:
+        await event.answer(f'This parcel open code is: {p.open_code}', alert=True)
+    else:
+        await event.answer(f'Parcel not ready for pickup!\nStatus: {p.status.value}', alert=True)
+
+
+async def open_comp(event, inp, shipment_number=None):
+    p: Parcel = await inp[event.sender.id].get_parcel(shipment_number=shipment_number, parse=True)
+
+    match p.status:
+        case ParcelStatus.DELIVERED:
+            await event.answer('Parcel already delivered!', alert=True)
+        case ParcelStatus.READY_TO_PICKUP:
+            await inp[event.sender.id].collect(parcel_obj=p)
+            await event.answer(
+                f'Compartment opened!\nLocation:\n   Side: {p.compartment_location.side}\n   Row: {p.compartment_location.row}\n   Column: {p.compartment_location.column}')
+        case _:
+            await event.answer(f'Parcel not ready for pickup!\nStatus: {p.status.value}', alert=True)
+
+
 async def main(config, inp: Dict):
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=config['log_level'])
     logger = logging.getLogger(__name__)
@@ -64,10 +119,12 @@ async def main(config, inp: Dict):
             await event.reply(e.reason)
         except UnauthorizedError:
             await event.reply('You are not authorized')
-        except NotFoundError:
-            await event.reply('User not found!')
-        except UnidentifiedAPIError:
+        except UnidentifiedAPIError as e:
+            logger.error(e)
             await event.reply('Unexpected error occurred, call admin')
+        except Exception as e:
+            logger.error(e)
+            await event.reply('Bad things happened, call admin now!')
 
     @client.on(NewMessage(pattern='/confirm'))
     async def confirm_sms(event):
@@ -77,18 +134,20 @@ async def main(config, inp: Dict):
                     await event.reply(f'Succesfully verifed!', buttons=[Button.inline('Pending Parcels'),
                                                                         Button.inline('Delivered Parcels')])
                 else:
-                    await event.reply('You fucked up')
+                    await event.reply('Could not confirm sms code!')
 
             except PhoneNumberError as e:
                 await event.reply(e.reason)
             except SmsCodeError as e:
                 await event.reply(e.reason)
             except UnauthorizedError:
-                await event.reply('You are not authorized')
-            except NotFoundError:
-                await event.reply('User not found!')
-            except UnidentifiedAPIError:
+                await event.reply('You are not authorized, initialize first!')
+            except UnidentifiedAPIError as e:
+                logger.error(e)
                 await event.reply('Unexpected error occurred, call admin')
+            except Exception as e:
+                logger.error(e)
+                await event.reply('Bad things happened, call admin now!')
         else:
             await event.reply('No sms code provided or not initialized')
 
@@ -103,11 +162,13 @@ async def main(config, inp: Dict):
             except RefreshTokenError as e:
                 await event.reply(e.reason)
             except UnauthorizedError:
-                await event.reply('You are not authorized')
-            except NotFoundError:
-                await event.reply('User not found!')
-            except UnidentifiedAPIError:
+                await event.reply('You are not authorized, initialize again')
+            except UnidentifiedAPIError as e:
+                logger.error(e)
                 await event.reply('Unexpected error occurred, call admin')
+            except Exception as e:
+                logger.error(e)
+                await event.reply('Bad things happened, call admin now!')
 
     @client.on(NewMessage(pattern='/parcel'))
     async def get_parcel(event):
@@ -124,7 +185,8 @@ async def main(config, inp: Dict):
                                   buttons=[
                                       [Button.inline('Open Code'),
                                        Button.inline('QR Code')],
-                                      [Button.inline('Open Compartment')]] if package.status != ParcelStatus.DELIVERED else
+                                      [Button.inline(
+                                          'Open Compartment')]] if package.status != ParcelStatus.DELIVERED else
                                   [Button.inline('Open Code'),
                                    Button.inline('QR Code')]
                                   )
@@ -132,11 +194,36 @@ async def main(config, inp: Dict):
             except NotAuthenticatedError as e:
                 await event.reply(e.reason)
             except UnauthorizedError:
-                await event.reply('You are not authorized, try refreshing token invoking /refresh')
-            except NotFoundError:
-                await event.reply('Parcel does not exist!')
-            except UnidentifiedAPIError:
+                if await inp[event.sender.id].refresh_token():
+                    try:
+                        package: Parcel = await inp[event.sender.id].get_parcel(
+                            shipment_number=event.text.split(' ')[1].strip(),
+                            parse=True)
+
+                        await event.reply(f'Sender: {package.sender.sender_name}\n'
+                                          f'Shipment number: {package.shipment_number}\n'
+                                          f'Status: {package.status.value}\n'
+                                          f'Pickup point: {package.pickup_point}',
+                                          buttons=[
+                                              [Button.inline('Open Code'),
+                                               Button.inline('QR Code')],
+                                              [Button.inline(
+                                                  'Open Compartment')]] if package.status != ParcelStatus.DELIVERED else
+                                          [Button.inline('Open Code'),
+                                           Button.inline('QR Code')]
+                                          )
+                    except Exception as e:
+                        logger.error(e)
+                        await event.reply('Bad things happened, call admin now!')
+                else:
+                    await event.reply('You are not authorized, initialize first!')
+
+            except UnidentifiedAPIError as e:
+                logger.error(e)
                 await event.reply('Unexpected error occurred, call admin')
+            except Exception as e:
+                logger.error(e)
+                await event.reply('Bad things happened, call admin now!')
         else:
             await event.reply('No shipment number provided or not initialized')
 
@@ -169,39 +256,33 @@ async def main(config, inp: Dict):
                 elif event.text == '/delivered':
                     status = ParcelStatus.DELIVERED
                 else:
-                    pass
+                    return
+
             try:
-                p: List[Parcel] = await inp[event.sender.id].get_parcels(status=status, parse=True)
-                if len(p) > 0:
-                    for package in p:
-                        await event.reply(f'Sender: {package.sender.sender_name}\n'
-                                          f'Shipment number: {package.shipment_number}\n'
-                                          f'Status: {package.status.value}\n'
-                                          f'Pickup point: {package.pickup_point}',
-                                          buttons=[
-                                              [Button.inline('Open Code'),
-                                               Button.inline('QR Code')],
-                                              [Button.inline('Open')]] if package.status != ParcelStatus.DELIVERED else
-                                          [Button.inline('Open Code'),
-                                           Button.inline('QR Code')]
-                                          )
-                else:
-                    if isinstance(event, CallbackQuery.Event):
-                        await event.answer('No parcels with specified status!', alert=True)
-                    elif isinstance(event, NewMessage.Event):
-                        await event.reply('No parcels with specified status!')
+                await send_pcgs(event, inp, status)
 
             except NotAuthenticatedError as e:
                 await event.reply(e.reason)
             except ParcelTypeError as e:
                 await event.reply(e.reason)
             except UnauthorizedError:
-                await event.reply('You are not authorized to fetch parcels! Try to refresh token `/refresh`')
+                if await inp[event.sender.id].refresh_token():
+                    try:
+                        await send_pcgs(event, inp, status)
+
+                    except Exception as e:
+                        logger.error(e)
+                        await event.reply('Bad things happened, call admin now!')
+                else:
+                    await event.reply('You are not authorized, initialize first!')
+
             except NotFoundError:
                 await event.reply('No parcels found!')
-            except UnidentifiedAPIError:
+            except UnidentifiedAPIError as e:
+                logger.error(e)
                 await event.reply('Unexpected error occurred, call admin')
-            except Exception:
+            except Exception as e:
+                logger.error(e)
                 await event.reply('Bad things happened, call admin now!')
 
         else:
@@ -213,21 +294,30 @@ async def main(config, inp: Dict):
             msg = await event.get_message()
             shipment_number = msg.raw_text.split('\n')[1].split(':')[1].strip()
             try:
-                p: Parcel = await inp[event.sender.id].get_parcel(shipment_number=shipment_number, parse=True)
-
-                await event.reply(file=p.generate_qr_image)
+                await send_qrc(event, inp, shipment_number)
 
             except NotAuthenticatedError as e:
                 await event.reply(e.reason)
             except ParcelTypeError as e:
                 await event.reply(e.reason)
             except UnauthorizedError:
-                await event.reply('You are not authorized! Try to refresh token `/refresh`')
+                if await inp[event.sender.id].refresh_token():
+                    try:
+                        await send_qrc(event, inp, shipment_number)
+
+                    except Exception as e:
+                        logger.error(e)
+                        await event.reply('Bad things happened, call admin now!')
+                else:
+                    await event.reply('You are not authorized, initialize first!')
+
             except NotFoundError:
                 await event.reply('Parcel not found!')
-            except UnidentifiedAPIError:
+            except UnidentifiedAPIError as e:
+                logger.error(e)
                 await event.reply('Unexpected error occurred, call admin')
-            except Exception:
+            except Exception as e:
+                logger.error(e)
                 await event.reply('Bad things happened, call admin now!')
         else:
             await event.reply('You are not initialized')
@@ -238,21 +328,29 @@ async def main(config, inp: Dict):
             msg = await event.get_message()
             shipment_number = msg.raw_text.split('\n')[1].split(':')[1].strip()
             try:
-                p: Parcel = await inp[event.sender.id].get_parcel(shipment_number=shipment_number, parse=True)
-
-                await event.answer(f'This parcel open code is: {p.open_code}', alert=True)
-
+                await show_oc(event, inp, shipment_number)
             except NotAuthenticatedError as e:
                 await event.reply(e.reason)
             except ParcelTypeError as e:
                 await event.reply(e.reason)
             except UnauthorizedError:
-                await event.reply('You are not authorized! Try to refresh token `/refresh`')
+                if await inp[event.sender.id].refresh_token():
+                    try:
+                        await show_oc(event, inp, shipment_number)
+
+                    except Exception as e:
+                        logger.error(e)
+                        await event.reply('Bad things happened, call admin now!')
+                else:
+                    await event.reply('You are not authorized, initialize first!')
+
             except NotFoundError:
                 await event.reply('Parcel not found!')
-            except UnidentifiedAPIError:
+            except UnidentifiedAPIError as e:
+                logger.error(e)
                 await event.reply('Unexpected error occurred, call admin')
-            except Exception:
+            except Exception as e:
+                logger.error(e)
                 await event.reply('Bad things happened, call admin now!')
 
         else:
@@ -261,33 +359,49 @@ async def main(config, inp: Dict):
     @client.on(CallbackQuery(pattern=b'Open Compartment'))
     async def open_compartment(event):
         if event.sender.id in inp:
+            await event.reply('Are you sure? This operation is irreversible!',
+                              buttons=[Button.inline('Yes!'), Button.inline('Hell no!')])
+
+        else:
+            await event.reply('You are not initialized')
+
+    @client.on(CallbackQuery(pattern=b'Yes!'))
+    async def yes(event):
+        if event.sender.id in inp:
             msg = await event.get_message()
+            msg = await msg.get_reply_message()
             shipment_number = msg.raw_text.split('\n')[1].split(':')[1].strip()
             try:
-                p: Parcel = await inp[event.sender.id].get_parcel(shipment_number=shipment_number, parse=True)
-
-                match p.status:
-                    case ParcelStatus.DELIVERED:
-                        await event.answer('Parcel already delivered!', alert=True)
-                    case ParcelStatus.READY_TO_PICKUP:
-                        await inp[event.sender.id].collect(parcel_obj=p)
-                    case _:
-                        await event.answer(f'Parcel not ready for pickup! Status: {p.status.value}', alert=True)
+                await open_comp(event, inp, shipment_number)
 
             except NotAuthenticatedError as e:
                 await event.reply(e.reason)
             except ParcelTypeError as e:
                 await event.reply(e.reason)
             except UnauthorizedError:
-                await event.reply('You are not authorized! Try to refresh token `/refresh`')
+                if await inp[event.sender.id].refresh_token():
+                    try:
+                        await open_comp(event, inp, shipment_number)
+
+                    except Exception as e:
+                        logger.error(e)
+                        await event.reply('Bad things happened, call admin now!')
+                else:
+                    await event.reply('You are not authorized, initialize first!')
             except NotFoundError:
                 await event.reply('Parcel not found')
-            except UnidentifiedAPIError:
+            except UnidentifiedAPIError as e:
+                logger.error(e)
                 await event.reply('Unexpected error occurred, call admin')
-            except Exception:
+            except Exception as e:
+                logger.error(e)
                 await event.reply('Bad things happened, call admin now!')
         else:
             await event.reply('You are not initialized')
+
+    @client.on(CallbackQuery(pattern=b'Hell no!'))
+    async def no(event):
+        await event.answer('Fine, compartment remains closed!')
 
     async with client:
         print("Good morning!")
